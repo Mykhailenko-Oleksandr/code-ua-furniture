7083715c4231adc456ee072e10e9e953f8574ddb
Oleg_Pancheshniy <oleg.pancheshniy@gmail.com>
2025-08-09 00:24:13 +0300

    refactoring html and css

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-/**
- * У файлі main.js логіка сторінки Index (index.html)
- */
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Accordion('.accordion-container', {
+    duration: 300,
+    showMultiple: false,
+  });
+});
